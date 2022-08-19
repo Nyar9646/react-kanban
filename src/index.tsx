05cc7a3dfa6aca14b66fc6d36372e8a22b1d5efa
工藤玲奈 <kudoureina@Nyar-MacBook-Air.local>
2022-08-19 21:13:43 +0900

    モック作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,138 +1,14 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import styled, {createGlobalStyle} from 'styled-components'
-import * as color from './color'
+import { GlobalStyle } from "./GlobalStyle"
+import { App } from "./App"
+
+// JSX は JS に変換される。html は js のみ扱えるため
+const elementJSX = <h1 title="hello">Hello JSX!</h1>
+const elementJS = React.createElement("h1", {title: "hello"}, "Hello JSX!")
 
 const container = document.getElementById('app')
 const root = createRoot(container!)
-
-const GlobalStyle = createGlobalStyle`
-  html, body, #app {
-    height: 100%;
-  }
-
-  body {
-    /* system font stack */
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    overflow-wrap: break-word;
-  }
-`
-
-function App() {
-  const Container = styled.div`
-    display: flex;
-    flex-flow: column;
-    height: 100%;
-  `
-  const Header = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 8px 16px;
-    background-color: ${color.Navy};
-    flex-shrink: 0;
-    color: ${color.Silver};
-    font-size: 16px;
-    font-weight: bold;
-  `
-  const Logo = styled.div`
-    height: 100%;
-    padding: 16px 0;
-    overflow-y: auto;
-  `
-  const CardFilter = styled.input`
-    display: flex;
-    align-items: center;
-    min-width: 300px;
-    border: solid 1px ${color.Silver};
-    border-radius: 3px;
-  `
-  const MainArea = styled.div`
-    height: 100%;
-    padding: 16px 0;
-    overflow-y: auto;
-  `
-  const HorizontalScroll = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    overflow-y: auto;
-
-    > * {
-      margin-left: 16px;
-      flex-shrink: 0;
-    }
-
-    ::after {
-      display: block;
-      flex: 0 0 16px;
-      content: '';
-    }
-  `
-  const Column = styled.div`
-    display: flex;
-    flex-flow: column;
-    width: 355px;
-    height: 100%;
-    border: solid 1px ${color.Silver};
-    border-radius: 6px;
-    background-color: ${color.LightSilver};
-
-    > :not(:last-child) {
-      flex-shrink: 0;
-    }
-  `
-  const ColumnHeader = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  `
-  const Card = styled.div`
-    position: relative;
-    border: solid 1px ${color.Silver};
-    border-radius: 6px;
-    box-shadow: 0 1px 3px hsla(0, 0%, 7%, 0.1);
-    padding: 8px 32px;
-    background-color: ${color.White};
-    cursor: move;
-  `
-
-  return (
-    <Container>
-      <Header>
-        <Logo>Kanban board</Logo>
-        <CardFilter placeholder="Filter cards" />
-      </Header>
-
-      <MainArea>
-        <HorizontalScroll>
-          <Column>
-            <ColumnHeader>TODO</ColumnHeader>
-            <Card>朝食をとる🍞</Card>
-            <Card>SNSをチェックする🐦</Card>
-            <Card>布団に入る (:3[___]</Card>
-          </Column>
-
-          <Column>
-            <ColumnHeader>Doing</ColumnHeader>
-            <Card>顔を洗う👐</Card>
-            <Card>歯を磨く🦷</Card>
-          </Column>
-
-          <Column>
-            <ColumnHeader>Waiting</ColumnHeader>
-          </Column>
-
-          <Column>
-            <ColumnHeader>Done</ColumnHeader>
-            <Card>布団から出る (:3っ)っ -=三[＿＿]</Card>
-          </Column>
-        </HorizontalScroll>
-      </MainArea>
-    </Container>
-  )
-}
 
 root.render(
   <>
